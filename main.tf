d5f124edd7e33cfe90d17e0d684f9006262f954d
thehouseplant <sean.collins@outlook.com>
2022-12-18 18:47:55 -0800

    Define basic vSphere datacenter resources

---
+++ main.tf
@@ -0,0 +1,18 @@
+data "vsphere_datacenter" "datacenter" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere_compute_cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
