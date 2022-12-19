16a9cf73191f70436495ec88e2e241a52f19d91f
thehouseplant <sean.collins@outlook.com>
2022-12-18 18:42:39 -0800

    Define Terraform and provider versions

---
+++ versions.tf
@@ -0,0 +1,10 @@
+terraform {
+    required_providers {
+        vsphere = {
+            source = "hashicorp/vsphere"
+            version = ">= 2.2.0"
+        }
+    }
+
+    required_version = ">= 0.14"
+}
