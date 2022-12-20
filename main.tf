fcf22afd64f62db6216fcfa8fc9bad68c8eb664b
thehouseplant <sean.collins@outlook.com>
2022-12-19 16:49:13 -0800

    Add single virtual machine resource

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Datacenter and other server data imports
 data "vsphere_datacenter" "datacenter" {
   name = var.vsphere_datacenter
 }
@@ -15,4 +16,26 @@ data "vsphere_compute_cluster" "cluster" {
 data "vsphere_network" "network" {
   name          = var.vsphere_network
   datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+
+
+# Virtual machine resources
+resource "vsphere_virtual_machine" "vm" {
+  name             = "test"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 1
+  memory   = 1024
+  guest_id = ""
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
 }
