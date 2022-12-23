84ca269f7de4a497a2b01860592b95288d723b2e
thehouseplant <sean.collins@outlook.com>
2022-12-23 00:12:36 -0800

    Add variable typings

--- variables.tf
+++ variables.tf
@@ -2,19 +2,23 @@
 variable "vsphere_user" {
   description = "Name of the vSphere user account"
   default     = ""
+  type        = string
 }
 
 variable "vsphere_password" {
   description = "Password for the vSphere user account"
   default     = ""
+  type        = string
 }
 
 variable "vsphere_server" {
   description = "Name of the vSphere server"
   default     = ""
+  type        = string
 }
 
 variable "vsphere_datacenter" {
   description = "URL of the vSphere datacenter"
   default     = ""
+  type        = string
 }
